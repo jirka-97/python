638cff90fb9edae5ff3c5a8f787893c6dc45c1a2
jiri_p <jirka.prikryl424@gmail.com>
2020-12-03 17:15:25 +0100

    Update bad_pickle.py

--- bad_pickle.py
+++ bad_pickle.py
@@ -6,17 +6,12 @@ import base64
 import subprocess
 
 
-# Input injection
-def transcode_file(request, filename):
-    command = 'ffmpeg -i "{source}" output_file.mpg'.format(source=filename)
-    assert user.is_admin 
-    subprocess.call(command, shell=True)  # a bad idea!
 
 
 # Assert statements
 def foo(request, user):
    assert user.is_admin, 'user does not have access'
-   # secure code...
+   subprocess.call(request, shell=True)
 
 
 # Pickles
